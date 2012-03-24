bf70cb865e68b6c9458832e94160aae9641f9275
Vinti <vinti.uiet@gmail.com>
2012-03-24 13:04:49 -0700

    Complete solution with tests

---
+++ README.txt
@@ -0,0 +1,12 @@
+Brief explanation of design and assumptions:
+
+This is a solution of Problem 2, Sales Tax. I worte it in Ruby and used Rspec and Rake for automated testing.
+To give intputs, there is input.txt file exist in the folder. 
+
+Assumptions to test solution?
+
+1- $ cd sales_tax
+2- Paste input into the input.txt file ( one by one)
+3- Check output from the command line by running the command: $ ruby sales_tax.rb
+4- To ensure rspec tests are working use: $ rake
+
--- sales_tax_spec.rb
+++ sales_tax_spec.rb
@@ -1,29 +1,43 @@
 require "sales_tax"
 
-describe "SalesTax" do
+describe "Reciept" do
   before :each do 
     @p1 = ["1 imported box of chocolates at 10.00", "1 imported bottle of perfume at 47.50"]
     @p2 = [ "1 book at 12.49", "1 music CD at 14.99", "1 chocolate bar at 0.85" ]
     @p3 = [ "1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99", "1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25"]
-    @st = SalesTax.new
+    @st = Receipt.new
+    @product = "1 imported bottle of perfume at 27.99"
   end
   
-  it "Sales tax amount" do
-    a, b = @st.receipt(@p1)
+  it "Total amount and total Sales Tax amount" do
+    a, b = @st.print_receipt(@p1)
     a.should == "7.65"
     b.should == "65.15"
     
-    a, b = @st.receipt(@p2)
+    a, b = @st.print_receipt(@p2)
     a.should == "1.50"
     b.should == "29.83"
 
-    a, b = @st.receipt(@p3)
+    a, b = @st.print_receipt(@p3)
     a.should == "6.65"
     b.should == "74.63"
   end
   
-  it "Amount" do
-
+  it "evaluating qty, rate  and product" do
+    qty, rate, product = @st.parse_input(@product)
+    qty.should == 1
+    product.should == "imported bottle of perfume"
+    rate.should == 27.99
+  end
+  
+  it "individual amount and tax amount" do
+    amount, tax_amount = @st.tax_calculations("imported bottle of perfume", 1, 27.99)
+    amount.should == 27.99
+    tax_amount.should == 4.2
+  end
+  
+  it "round_amount" do
+    @st.round_amount(6.6444).should == 6.65 
   end
   
 end
